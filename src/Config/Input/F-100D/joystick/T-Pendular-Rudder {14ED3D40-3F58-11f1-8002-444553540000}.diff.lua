6fca8f4e234fdb29cff3d196c61cac8d2614dbcb
Scott McIntosh <scottymac40love@gmail.com>
2026-09-13 15:40:12 -0700

    Scaffolded with IPI 3.10, removed TM Warthog Throttle

--- src/Config/Input/F-100D/joystick/T-Pendular-Rudder {14ED3D40-3F58-11f1-8002-444553540000}.diff.lua
+++ src/Config/Input/F-100D/joystick/T-Pendular-Rudder {14ED3D40-3F58-11f1-8002-444553540000}.diff.lua
@@ -1,29 +1,23 @@
 local diff = {
-	["axisDiffs"] = {
-		["a2003cdnil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_Z",
-				},
-			},
-			["name"] = "Rudder",
-		},
-		["a3006cd9"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_Y",
-				},
-			},
-			["name"] = "Wheel Brake Left",
-		},
-		["a3007cd9"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_X",
-				},
-			},
-			["name"] = "Wheel Brake Right",
-		},
-	},
+  ["axisDiffs"] = {
+    ["a2003cdnil"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_Z" },
+      },
+      ["name"] = "Rudder",
+    },
+    ["a3006cd9"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_Y", ["filter"] = { ["invert"] = true } },
+      },
+      ["name"] = "Wheel Brake Left",
+    },
+    ["a3007cd9"] = {
+      ["added"] = {
+        [1] = { ["key"] = "JOY_X", ["filter"] = { ["invert"] = true } },
+      },
+      ["name"] = "Wheel Brake Right",
+    },
+  },
 }
 return diff
